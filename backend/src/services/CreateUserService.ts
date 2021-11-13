d187c1afb582fd7ec7e311b9108323559061b961
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-11-13 19:52:55 -0300

    Criando as regras de aplicação para os usuários

---
+++ backend/src/services/CreateUserService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+import { hash } from 'bcryptjs';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+  password: string;
+}
+
+export class CreateUserService {
+
+  async execute({ name, email, admin, password }: IUserRequest) {
+    const userRepository = getCustomRepository(UsersRepositories);
+
+    if (!email) {
+      throw new Error("Incorrect e-mail!");
+    }
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      admin,
+      password: passwordHash,
+    });
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}

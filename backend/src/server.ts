489dd1a38c84b4b6678aabaadf421757109fe2d0
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-11-14 15:44:41 -0300

    Instalando o cors no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 import { router } from './routes';
 import './database';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(router);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+
 export function App() {
   return (
     <h1>Iniciando o frontend com React</h1>
